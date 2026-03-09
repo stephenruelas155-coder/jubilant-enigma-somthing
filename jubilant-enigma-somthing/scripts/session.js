18eb6a193ad5eca4c70b38f546cbde36f1fd9e3f
stephenruelas155-coder <stephenruelas155@gmail.com>
2026-03-09 00:34:39 -0500

    Delete jubilant-enigma-somthing directory

--- jubilant-enigma-somthing/scripts/session.js
+++
@@ -1,8 +0,0 @@
-
-function isAuthentication() {
-    console.log('isAuthentication?', sessionStorage.getItem('isAuthentication'));
-    return sessionStorage.getItem('isAuthentication') === 'true';
-}
-
-document.addEventListener('DOMContentLoaded', isAuthentication);
-
